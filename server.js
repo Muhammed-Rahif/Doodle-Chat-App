8c4d90a4780aa28740e8670cb7409ccb94cf64e7
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-01-25 13:53:52 +0530

    Doodle-Chat-App project initialized

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Page content")
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server started running at port "${port}"`);
+})
