88dffc536747ded37dfd2d5c9f6496d4e33e2d04
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-01-25 15:44:58 +0530

    Home page and login page designed

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express();
 const port = 3000;
 const path = require('path')
+const session = require('express-session')
+const bodyParser = require('body-parser');
 
 // hbs implement
 var handlebars = require('express-handlebars').create({ extname: '.hbs' });
@@ -14,14 +16,33 @@ app.set('view engine', 'hbs')
 
 app.use(express.static(`${__dirname}/public/`))
 
+// Express-session middleware
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 } }))
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 
 
 // Routers
-
 app.get('/', (req, res) => {
-    res.render(`users/chat-home`)
+    let user = req.session.userData
+    if (user) {
+        res.render(`users/chat-home`, { user })
+    } else {
+        res.redirect('/login')
+    }
 })
+app.get('/login', (req, res) => {
+    res.render('users/login-page')
+})
+app.post('/login', (req, res) => {
+    let loginData = req.body;
+    req.session.userData = loginData;
+    res.redirect('/')
+})
+
 
 
 
