852ff9d6ec0c42f61cf64f8c15ea07052841c95a
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-01-25 23:29:58 +0530

    Any username connecting or disconnecting to public room will display in public room

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const path = require('path')
 const session = require('express-session')
 const bodyParser = require('body-parser');
 
+// Server listening
+const server = app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
+    console.log(`http://localhost:${port}`);
+});
+
+// Socket.io
+const io = require('socket.io')(server);
+
 // hbs implement
 var handlebars = require('express-handlebars').create({ extname: '.hbs' });
 app.engine('hbs', handlebars.engine);
@@ -17,7 +26,7 @@ app.set('view engine', 'hbs')
 app.use(express.static(`${__dirname}/public/`))
 
 // Express-session middleware
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 } }))
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 600000 } }))
 
 // Body parser
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -35,20 +44,50 @@ app.get('/', (req, res) => {
     }
 })
 app.get('/login', (req, res) => {
-    res.render('users/login-page')
+    let user = req.session.userData
+    if (user) {
+        res.redirect('/')
+    } else {
+        res.render('users/login-page')
+    }
 })
 app.post('/login', (req, res) => {
     let loginData = req.body;
     req.session.userData = loginData;
     res.redirect('/')
 })
-
-
-
-
-
-
-// Server listening
-app.listen(port, () => {
-    console.log(`Server started running at port "${port}"`);
+app.get('/logout', (req, res) => {
+    req.session.userData = null;
+    res.redirect('/')
 })
+
+
+
+// Socket.io
+io.on("connection", function (socket) {
+    console.log(`A user connected`);
+
+    // All users deatials
+    var allUsers = [];
+    function searchUser(userId){
+        for (var i=0; i < allUsers.length; i++) {
+            if (allUsers[i].id === userId) {
+                return allUsers[i];
+            }
+        }
+    };
+
+    socket.on("disconnect",async () => {
+        console.log("A user disconnected");
+        var userData = await searchUser(socket.id);
+        io.emit('userDisconnect', userData.name);
+    });
+    
+    socket.on("userConnected",(userData)=>{
+        allUsers.push(userData);
+        io.emit('userConnect', userData);
+    });
+
+    
+});
+
