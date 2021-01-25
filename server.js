9d12d82eee930acf6597671635956c2cbf2c41a0
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-01-25 14:47:15 +0530

    Handlebars html view template integrated

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
+// Express implement
 const express = require('express')
 const app = express();
 const port = 3000;
+const path = require('path')
+
+// hbs implement
+var handlebars = require('express-handlebars').create({ extname: '.hbs' });
+app.engine('hbs', handlebars.engine);
+
+// View and view engine setup
+app.set('views', path.join(`${__dirname}/views`))
+app.set('view engine', 'hbs')
+
+app.use(express.static(`${__dirname}/public/`))
+
+
+
+
+// Routers
 
 app.get('/', (req, res) => {
-    res.send("Page content")
+    res.render(`users/chat-home`)
 })
 
 
 
 
 
-
+// Server listening
 app.listen(port, () => {
     console.log(`Server started running at port "${port}"`);
 })
